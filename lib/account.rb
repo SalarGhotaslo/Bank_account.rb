822522f0a95bdca94f4d7cf160ae0dd89326e122
Salar Ghotaslo <Salar_@live.co.uk>
2021-03-08 18:33:41 +0000

    Customer is able to deposit into their account

--- lib/account.rb
+++ lib/account.rb
@@ -4,4 +4,8 @@ class Account
         def initialize
             @balance = 0
         end
+
+        def deposit(amount)
+            @balance = balance + amount
+        end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,8 +2,12 @@ require 'account'
 
 describe 'Account' do
     account = Account.new
-    it 'should have a balanve of 0 before any transactions have been made' do
+    it 'should have a balance of 0 before any transactions have been made' do
     expect(account.balance).to eq (0)
+    end
+    it 'should deposit money into my account' do
+        account.deposit(500)
+        expect(account.balance).to eq(500)
     end
     
 end
